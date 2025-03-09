572341671a2c73b069d24eaeca52ffcf816f1b61
Wasifa Fatima <wasifafatima123@gmail.com>
2025-03-10 03:04:59 +0600

    final

--- blog.js
+++ blog.js
@@ -2,4 +2,4 @@ let backToDesk = document.getElementById("back-to-desk")
     .addEventListener("click", function(){
         window.location.href = "index.html";  
     })
-// Going back to home page
+
